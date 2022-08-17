ebae70fa2744d9a67a0cd600cac8700f0ccc0cc8
manish mandal <76776194mm@gmail.com>
2022-08-18 00:08:54 +0530

    update js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 `use strict`
 
 const shareBtn=document.querySelector('.container__share-icon-block ');
+const lastSection=document.querySelector('.container__last-section');
+const jsFooter=document.querySelector('.social-media-block');
+shareBtn.addEventListener('click' , function(){
+    jsFooter.classList.remove('hidden');
+    lastSection.classList.add('hidden');
+})
