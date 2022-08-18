c3bd05332412d6d117892fa97bf904b28336e6a0
manish mandal <76776194mm@gmail.com>
2022-08-18 18:43:24 +0530

    update js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 `use strict`
 
 const shareBtn=document.querySelector('.container__share-icon-block ');
-const lastSection=document.querySelector('.container__last-section');
+// const lastSection=document.querySelector('.container__last-section');
 const jsFooter=document.querySelector('.social-media-block');
+const shareBlock=document.querySelector('.share-block');
 
 shareBtn.addEventListener('click' , function(){
-    jsFooter.classList.remove('hidden');
+    jsFooter.classList.toggle('hidden');
+})
+
+shareBlock.addEventListener('click',function(){
+    jsFooter.classList.add('hidden');
 })
